7152fbda685f842e75fdcfce9523ee402b5f243b
mohamed-farouk07 <60257984+mohamed-farouk07@users.noreply.github.com>
2024-10-20 16:20:32 +0200

    .

---
+++ shopping-cart/src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+// components/Header.tsx
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-gray-800 shadow-md p-5">
+      <h1 className=" text-2xl font-bold text-white">
+        Shopping Cart
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ shopping-cart/src/app/page.tsx
@@ -0,0 +1,10 @@
+// src/app/page.tsx
+import Header from "./components/Header";
+
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <Header />
+    </div>
+  );
+}
